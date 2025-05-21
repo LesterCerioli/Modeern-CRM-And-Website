83f31955365eb3e9090d4aa9732b1dd9d6888dfb
LesterCerioli <lesterlucasit@hotmail.com>
2025-05-21 19:21:42 -0400

    issue(#64): Building candidates POST endpoint

--- src/app/api/candidates/route.js
+++ src/app/api/candidates/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { z } from 'zod';
-import { CandidateService } from '@/services/candidateService';
+
 import candidateService from '../../../../services/candidateService';
 
 const candidateService = new CandidateService();
