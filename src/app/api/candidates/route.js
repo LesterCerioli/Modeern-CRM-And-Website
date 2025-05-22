910604efe1ef0b171b284c1b30163f9ceaf30341
LesterCerioli <lesterlucasit@hotmail.com>
2025-05-22 11:43:20 -0400

    issue(#64): Building candidates POST endpoint

--- src/app/api/candidates/route.js
+++ src/app/api/candidates/route.js
@@ -1,6 +1,5 @@
-// app/api/candidate/route.js
 
-import { v4 as uuidv4 } from 'uuid';
+
 import CandidateService from "@/services/candidateService";
 
 const candidateService = new CandidateService();
@@ -8,16 +7,12 @@ const candidateService = new CandidateService();
 export async function GET() {
     try {
         const candidates = await candidateService.findAll();
-
-        
         return new Response(
             JSON.stringify(candidates),
             { status: 200, headers: { "Content-Type": "application/json" } }
         );
     } catch (error) {
         console.error("Candidate API Error:", error);
-
-
         return new Response(
             JSON.stringify({ error: "Internal Server Error" }),
             { status: 500, headers: { "Content-Type": "application/json" } }
@@ -28,19 +23,8 @@ export async function GET() {
 export async function POST(req) {
     try {
         const body = await req.json();
+        const requiredFields = ["firstName", "lastName", "email", /* ... */];
 
-
-        const requiredFields = [
-            "firstName",
-            "lastName",
-            "email",
-            "telephone",
-            "city",
-            "state",
-            "country",
-            "passportId",
-            "linkedinUrl"
-        ];
 
         for (const field of requiredFields) {
             if (!body[field]) {
@@ -60,31 +44,18 @@ export async function POST(req) {
             );
         }
 
-        const candidateData = {
-            id: uuidv4(),
+
+        const newCandidate = await candidateService.create({
+            id: uuid(),
             ...body
-        };
-
-        const newCandidate = await candidateService.create(candidateData);
-
+        });
 
         return new Response(
             JSON.stringify({ message: "Candidate created successfully", candidate: newCandidate }),
             { status: 201, headers: { "Content-Type": "application/json" } }
         );
-
     } catch (error) {
         console.error("Candidate API Error:", error);
-
-
-        if (error.message === "Candidate not found") {
-            return new Response(
-                JSON.stringify({ error: "Candidate not found" }),
-                { status: 404, headers: { "Content-Type": "application/json" } }
-            );
-        }
-
-
         return new Response(
             JSON.stringify({ error: "Internal Server Error" }),
             { status: 500, headers: { "Content-Type": "application/json" } }
