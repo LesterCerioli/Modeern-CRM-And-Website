aaeefd128322760290ae6529b4caa584a5850bec
LesterCerioli <lesterlucasit@hotmail.com>
2025-05-22 08:55:38 -0400

    issue(#64): Building candidates POST endpoint

--- src/app/services/candidateService.ts
+++ src/app/services/candidateService.ts
@@ -1,4 +1,5 @@
 import { CandidateDTO } from "@/domain/dtos/candidateDTO";
+import { CandidateContractService } from "./candidateContractService";
 
 export class CandidateService implements CandidateContractService {
 
@@ -58,5 +59,5 @@ export class CandidateService implements CandidateContractService {
   }
 
 
-  
+
 }
